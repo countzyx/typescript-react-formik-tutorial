e9fbe733a31fd34cde773d0066328a46db18b479
Pedro R. Jiménez <ptpedroj@gmail.com>
2021-03-11 02:00:08 -0500

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const render = (element: HTMLElement) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    element
+  );
+};
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  if (module.hot) {
+    module.hot.accept("./App/Layout/App", () => setTimeout(render));
+  }
+
+  render(rootElement);
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
