e42165a3e5744b3dc3c812e3124c6383d061d5b0
Pedro R. Jiménez <ptpedroj@gmail.com>
2021-03-11 17:39:58 -0500

    Added Validation
    Validation errors are not displaying automatically. However the form
    submission is getting blocked and the error state is actively updated.

--- src/Features/SignupForm/SignupForm.tsx
+++ src/Features/SignupForm/SignupForm.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
+
+const kRequired = 'Required';
 
 type FormValues = {
   email: string;
@@ -14,9 +16,42 @@ const SignupForm: React.FC = () => {
     lastName: '',
   };
 
+  const validate = (values: FormValues): FormikErrors<FormValues> => {
+    const errors: FormikErrors<FormValues> = {};
+
+    if (!values.email?.trim()) {
+      errors.email = kRequired;
+    } else if (
+      // eslint-disable-next-line no-control-regex
+      !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9](?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+        values.email,
+      )
+    ) {
+      errors.email = 'Invalid email address';
+    }
+
+    const firstName = values.firstName?.trim();
+    if (!firstName) {
+      errors.firstName = kRequired;
+    } else if (firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    const lastName = values.lastName?.trim();
+    if (!lastName) {
+      errors.lastName = kRequired;
+    } else if (lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues,
+    validate,
     onSubmit: (values) => {
+      // eslint-disable-next-line no-alert
       alert(JSON.stringify(values, null, 2));
     },
   });
